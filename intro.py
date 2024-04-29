9c1c7f85349cd575c7b18cea0d4d04a8a9c650c5
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-04-29 16:37:12 -0500

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Recopilación del Semestre, Sebastian Ramirez")
+st.subheader("Esta página te permitirá ver el progreso del trabajo en clase de inteligencia computacional")
+st.image("DSC09768.JPG")
