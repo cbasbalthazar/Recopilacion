af4a44a8fd66742cbbaa517f7f9c4736d4b9f3c5
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-04-29 16:40:35 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,4 +1,3 @@
 import streamlit as st
 st.title("Recopilación del Semestre, Sebastian Ramirez")
-st.subheader("Esta página te permitirá ver el progreso del trabajo en clase de inteligencia computacional")
-st.image("DSC09768.JPG")
+st.subheader("Acá se encuentra la recopilación de trabajos de Inteligencia Urbana")
