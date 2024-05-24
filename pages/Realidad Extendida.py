00a13b38f602cd75c8f830a727fd3319cfd0a3a3
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-05-23 22:21:20 -0500

    Update Realidad Extendida.py

--- pages/Realidad Extendida.py
+++ pages/Realidad Extendida.py
@@ -6,3 +6,5 @@ from PIL import Image
 st.title('Realidades extendidas')
 #image = Image.open('Panel Control.png')
 st.image(image)
+
+st.write('En este link podrás encontrarte un maravilloso mundo de realidades extendidas que te llevan por un recorrido de los contenidos estudiados en inteligencia urbana, un curso del pregrado en Diseño Urbano y Gestión del Hábitat')
